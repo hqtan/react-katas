16f3266928532f5db80aa6fd209af4532549208c
hqtan <han-qing.tan@rea-group.com>
2021-02-22 15:07:25 +1100

    refactored code using suggestions of Kris.

--- src/widget.js
+++ src/widget.js
@@ -1,28 +1,23 @@
 import React, { useState } from 'react';
 
 const Widget = () => {
-  return (
-    <div>
-      <Counter />
-    </div>
-  );
+  return <Counter /> ;
 };
 
 const Counter = () => {
-  const [count, inc] = useState(0);
+  const [count, setCount] = useState(0);
 
   // const addOne = n => e => inc(count + n);
   function add(n) {
-    inc(count + n);
+    setCount(count + n);
   }
-  const minusOne = () => inc(count - 1);
 
   return (
-    <div>
-      <button onClick = { () => add(2) }>Add</button>
-      <button onClick = { minusOne }>Minus</button>
+    <>
+      <button onClick = { () => add(1) }>Add</button>
+      <button onClick = { () => add(-1) }>Minus</button>
       <p>{ count }</p>
-    </div>
+    </>
   )
 }
 
