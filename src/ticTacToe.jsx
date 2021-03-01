4f5e6d387de2b6b62dd95233ff2aca23d0618aa6
hqtan <han-qing.tan@rea-group.com>
2021-03-01 23:50:07 +1100

    update square to display 'X' on click - update css styling to embiggen and center font in div

--- src/ticTacToe.jsx
+++ src/ticTacToe.jsx
@@ -1,15 +1,30 @@
+import React, { useState } from 'react';
 import './ticTacToe.css';
 
-const Square = () => (
-  <div
-    className="square"
-    onClick={() => console.log('clicked square')}
-    onKeyDown={() => console.log('key pressed')}
-    role="button"
-    tabIndex="0"
-  >
-    X
-  </div>
-);
+const Square = () => {
+  const [xo, setXo] = useState('');
+
+  const handleClick = () => {
+    console.log('clicked square');
+    setXo('X');
+  };
+
+  const handleKeyDown = () => {
+    console.log('key pressed');
+    setXo('X');
+  };
+
+  return (
+    <div
+      className="square"
+      onClick={() => handleClick()}
+      onKeyDown={() => handleKeyDown()}
+      role="button"
+      tabIndex="0"
+    >
+      { xo }
+    </div>
+  );
+};
 
 export default Square;
