ada778c3476103183dc576aaed19eabbc044ac71
hqtan <han-qing.tan@rea-group.com>
2021-03-06 17:34:47 +1100

    add player state

--- src/ticTacToe.jsx
+++ src/ticTacToe.jsx
@@ -1,18 +1,32 @@
 import React, { useState } from 'react';
 import './ticTacToe.css';
 
+function updatePlayerState(row, col, pState, setPState) {
+  const plyrState = {
+    ownedRows: pState.ownedRows,
+    ownedCols: pState.ownedCols,
+  };
+  plyrState.ownedRows[row] = true;
+  plyrState.ownedCols[col] = true;
+  console.log(`player state:
+    ownedRows: ${plyrState.ownedRows}, ownedCols: ${plyrState.ownedCols}`);
+  setPState(plyrState);
+}
+
 const Square = (coords) => {
   const [xo, setXo] = useState({ mark: '', filled: false });
 
   const handleClick = () => {
     console.log(`clicked square on row:${coords.row}, col:${coords.col}, 
       previous state: mark: ${xo.mark}, filled:${xo.filled}`);
+    updatePlayerState(coords.row, coords.col, coords.plyrState, coords.updatePlyr);
     setXo({ mark: 'X', filled: true });
   };
 
   const handleKeyDown = () => {
     console.log(`key pressed on row:${coords.row}, col:${coords.col}, 
       previous state: mark: ${xo.mark}, filled:${xo.filled}`);
+    updatePlayerState(coords.row, coords.col, coords.plyrState, coords.updatePlyr);
     setXo({ mark: 'X', filled: true });
   };
 
@@ -29,27 +43,35 @@ const Square = (coords) => {
   );
 };
 
-const Board = () => (
-  <>
-    {/* top */}
-    <div className="row">
-      <Square row="0" col="0" />
-      <Square row="0" col="1" />
-      <Square row="0" col="2" />
-    </div>
-    {/* middle */}
-    <div className="row">
-      <Square row="1" col="0" />
-      <Square row="1" col="1" />
-      <Square row="1" col="2" />
-    </div>
-    {/* bottom */}
-    <div className="row">
-      <Square row="2" col="0" />
-      <Square row="2" col="1" />
-      <Square row="2" col="2" />
-    </div>
-  </>
-);
+const TicTacToe = () => {
+  const [plyr, setPlyr] = useState({
+    ownedRows: Array(3).fill(false),
+    ownedCols: Array(3).fill(false),
+  });
 
-export default Board;
+  return (
+    <>
+      {/* top */}
+      <div className="row">
+        <Square row="0" col="0" updatePlyr={setPlyr} plyrState={plyr} />
+        <Square row="0" col="1" updatePlyr={setPlyr} plyrState={plyr} />
+        <Square row="0" col="2" updatePlyr={setPlyr} plyrState={plyr} />
+      </div>
+      {/* middle */}
+      <div className="row">
+        <Square row="1" col="0" updatePlyr={setPlyr} plyrState={plyr} />
+        <Square row="1" col="1" updatePlyr={setPlyr} plyrState={plyr} />
+        <Square row="1" col="2" updatePlyr={setPlyr} plyrState={plyr} />
+      </div>
+      {/* bottom */}
+      <div className="row">
+        <Square row="2" col="0" updatePlyr={setPlyr} plyrState={plyr} />
+        <Square row="2" col="1" updatePlyr={setPlyr} plyrState={plyr} />
+        <Square row="2" col="2" updatePlyr={setPlyr} plyrState={plyr} />
+      </div>
+    </>
+  );
+};
+
+// export default Board;
+export default TicTacToe;
