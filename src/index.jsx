b1a7cf87b07d6f16e201f6ef89f33febce45742c
hqtan <han-qing.tan@rea-group.com>
2021-03-09 22:42:51 +1100

    replace `Board` with `TicTacToe` component

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 // import Widget from './widget';
-import Board from './ticTacToe';
+import TicTacToe from './ticTacToe';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <Widget /> */}
-    <Board />
+    <TicTacToe />
   </React.StrictMode>,
   document.getElementById('root'),
 );
